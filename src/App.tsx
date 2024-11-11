fd99b16b48dd4ff2459dc3b8b0487be987ec83ac
necco-neko <nekonecconeko@gmail.com>
2024-11-11 20:33:57 +0900

    おおまかな見た目を形成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <h1 className='title'>数独解答アプリ</h1>
+      {/*<InputForm />*/}
+      <div className='sudoku-grid'>
+        {Array.from({ length: 81 }, (_, index) => (
+          <input
+            key={index}
+            type="text"
+            className="sudoku-cell"
+            maxLength={1}
+          />
+        ))}
+      </div>
+      <div className='button-container'>
+        <button className='reset-button'>リセット</button>
+        <button className='solve-button'>解答</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
